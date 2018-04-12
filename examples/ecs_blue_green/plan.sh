4ad215a4ae52724e9f753c8efc73f983515867ab
Chris Williams <chris.williams@helix.com>
2018-04-12 11:25:12 -0700

    Edit plan example

--- examples/ecs_blue_green/plan.sh
+++ examples/ecs_blue_green/plan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-. ../../plan_helpers.sh
+. ../plan_helpers.sh
 
 begin shared
 run terracanary plan -S shared
@@ -11,6 +11,8 @@ if ! terracanary list | grep -q shared; then
     echo "Can't plan anything except shared stack until shared stack is built."
     exit
 fi
+
+NEXT_VERSION=$(terracanary next)
 
 begin code
 run terracanary plan -s code:$NEXT_VERSION -I shared
@@ -25,8 +27,24 @@ fi
 CURRENT_MAIN="main:$(terracanary output -S routing main_stack_version)" || CURRENT_MAIN=""
 MAIN_INPUTS="-i $CODE -I shared"
 
-plan_main
+if [[ $CURRENT_MAIN ]]; then
+    echo "Showing changes to existing main stack; note that deploy.sh may actually apply these changes by creating a new stack version."
+    begin main
+    run terracanary plan -s $CURRENT_MAIN $MAIN_INPUTS
+    end main
+else
+    echo "No currently deployed main stack; will build a new one."
+    begin main
+    run terracanary plan -s main:$NEXT_VERSION $MAIN_INPUTS
+    end main
+fi
 
-ROUTING_INPUTS="-i $CURRENT_MAIN"
-plan_routing
+if [[ $CURRENT_MAIN ]]; then
+    echo "Showing changes to routing assuming no new main stack version; actual deployment may change main stack version."
+    begin routing
+    run terracanary plan -S routing -i $CURRENT_MAIN
+    end routing
+else
+    echo "Can't plan routing stack until there is a main stack."
+fi
 
